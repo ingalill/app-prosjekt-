a85c6d918525d62490680b8e5ed51a627c933fd1
Christer Bakken <christer.bakken@gmail.com>
2016-10-27 11:39:56 +0200

    Opprettet activiteter
    Aktiviteter MainActivity og JobSeekerActivity

--- prosjektfil/app/src/main/java/no/ntnu/asd/prosjektfil/MainActivity.java
+++ prosjektfil/app/src/main/java/no/ntnu/asd/prosjektfil/MainActivity.java
@@ -1,13 +1,30 @@
 package no.ntnu.asd.prosjektfil;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button jobSekrButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        jobSekrButton = (Button) findViewById(R.id.jobseekerButton);
+
+        jobSekrButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, JobSeekerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
