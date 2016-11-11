12572d9aa25fd2012cf77a674a7650758443048d
martinblom <martinblom@live.no>
2016-11-11 09:48:53 +0100

    CameraActivity update

--- prosjektfil/app/src/main/java/no/ntnu/asd/prosjektfil/CameraActivity.java
+++ prosjektfil/app/src/main/java/no/ntnu/asd/prosjektfil/CameraActivity.java
@@ -1,13 +1,53 @@
 package no.ntnu.asd.prosjektfil;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class CameraActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        Button button = (Button) findViewById(R.id.takePhotoBtn);
+        imageView = (ImageView) findViewById(R.id.photoView);
+
+        //Disable the button if the user has no camera
+        if(!hasCamera())
+            button.setEnabled(false);
+    }
+
+    //Check if the user has a camera
+    private boolean hasCamera(){
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
+    }
+
+    //Launching the camera
+    public void launchCamera(View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //Take a picture and pass results along to onActivityResult
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    //If you want to return the image taken
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            //Get the photo
+            Bundle extras = data.getExtras();
+            Bitmap photo = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(photo);
+        }
     }
 }
