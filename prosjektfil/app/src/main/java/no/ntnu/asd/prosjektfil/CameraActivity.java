d22b32c77b9f630828621dfc3388bf9fb5aabf4d
martinblom <martinblom@live.no>
2016-11-18 12:06:20 +0100

    CameraActivity
    Konvertering av bilde til string og tilbake

--- prosjektfil/app/src/main/java/no/ntnu/asd/prosjektfil/CameraActivity.java
+++ prosjektfil/app/src/main/java/no/ntnu/asd/prosjektfil/CameraActivity.java
@@ -3,6 +3,7 @@ package no.ntnu.asd.prosjektfil;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -43,6 +44,8 @@ public class CameraActivity extends AppCompatActivity {
         startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
     }
 
+
+
     //If you want to return the image taken
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -50,13 +53,23 @@ public class CameraActivity extends AppCompatActivity {
             //Get the photo
             Bundle extras = data.getExtras();
             Bitmap photo = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(photo);
+            //imageView.setImageBitmap(photo);
 
+            // Convert the Bitmap image to byte array.
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             photo.compress(Bitmap.CompressFormat.JPEG, 100, baos);
             byte[] b = baos.toByteArray();
-
+            // Convert the byte array to string via Base64.
             String encodedImage = Base64.encodeToString(b , Base64.DEFAULT);
+            //System.out.print("Image.toString" + encodedImage);
+            System.out.print("String lengde: ");
+            System.out.println(encodedImage.length());
+
+            // Convert Base64 string to byte array
+            byte[] bb = Base64.decode(encodedImage , Base64.DEFAULT);
+            // Convert byte array to Bitmap
+            Bitmap bitmap = BitmapFactory.decodeByteArray(bb, 0, bb.length);
+            imageView.setImageBitmap(bitmap);
         }
     }
 }
